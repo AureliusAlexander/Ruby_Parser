263c0017605b76377bbe5bef24151f1523b39896
Alexander <alexander@pc-on.de>
2017-01-07 16:43:30 +0100

    Readied the parser file tried to make an example and reduced to 1 error

--- Parser.rb
+++ Parser.rb
@@ -1,7 +1,8 @@
 # Ruby Parser für ein Textadventure
+
 require 'treetop'
 
-base_path = File.expand_path(File."C:\Users\Alexander\Desktop\Schule\Info\FA\Programm"(__FILE__))
+base_path = File.expand_path(File.dirname("C:\Users\Alexander\Desktop\Schule\Info\FA\Programm"))
 
 require File.join(base_path, 'node_extensions.rb')
 
@@ -11,13 +12,27 @@ class Parser
 	@@parser = SexpParser.new
 	
 	def self.parse(data)
-		tree = @@parse(data)
+		tree = @@parser.parse(data)
 		
 		if(tree.nil?)
 			raise Exception, "Parse error at offset: #{@@parser.index}"
 		end
 		
-		return tree
+		self.clean_tree(tree)
+		
+		return tree.to_array
 	end
 	
+	private
+		
+		def self.clean_tree(root_node)
+			return if(root_node.elements.nil?)
+			root_node.elements.delete_if{|node| node.class.name == "Treetop::Runtime::Syntaxnode" }
+			root_node.elements.each {|node| self.clean_tree(node)}
+		end
+	
 end
+
+Parser.parse('this is a test 1 2.3')
+
+gets.chomp
