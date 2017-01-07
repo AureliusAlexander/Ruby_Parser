b39d38f9dc68fc8d1757ef4b47fb3e7c6faf9295
Alexander <alexander@pc-on.de>
2017-01-07 16:45:17 +0100

    Same changes as commit before

--- node_extensions.rb
+++ node_extensions.rb
@@ -2,22 +2,40 @@
 
 module Sexp
 
-	class IntegerLiteral < Treetop::Runtime::Syntaxnode
+	class IntegerLiteral < Treetop::Runtime::SyntaxNode
+		def to_array
+			return self.text_value.to_i
+		end
 	end
 	
-	class StringLiteral < Treetop::Runtime::Syntaxnode
+	class StringLiteral < Treetop::Runtime::SyntaxNode
+		def to_array
+			return eval self.text_value
+		end
 	end
 	
-	class FloatLiteral < Treetop::Runtime::Syntaxnode
+	class FloatLiteral < Treetop::Runtime::SyntaxNode
+		def to_array
+			return eval self.text_value
+		end
 	end
 	
-	class Identifier < Treetop::Runtime::Syntaxnode
+	class Identifier < Treetop::Runtime::SyntaxNode
+		def to_array
+			return eval self.text_value.to_f
+		end
 	end
 	
-	class Expression < Treetop::Runtime::Syntaxnode
+	class Expression < Treetop::Runtime::SyntaxNode
+		def to_array
+			return self.elements[0].to_array
+		end
 	end
 	
-	class Body < Treetop::Runtime::Syntaxnode
+	class Body < Treetop::Runtime::SyntaxNode
+		def to_array
+			return self.elements.map {|x| x.to_array}
+		end
 	end
 
 end
